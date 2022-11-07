875edb125738e4a72b1d35fd715d6c4b6c3cd779
david <leachda12@gmail.com>
2022-11-07 15:45:25 -0500

    some icon tweaks

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 $('document').ready(function () {
-
+    const flashlight = $('.flashlight')
     $('.head').mousemove(e => {
+        
         e.target.style.setProperty('--xpos', `${e.pageX}px`);
         e.target.style.setProperty('--ypos', `${e.pageY}px`);
-        $('.glow').css('left', `${e.pageX}px`);
-        $('.glow').css('top', `${e.pageY}px`);
+        flashlight.css('left', `${e.pageX}px`);
+        flashlight.css('top', `${e.pageY}px`);
     });
 
-    $('.fake-power').hover(e => {
-        $('.fake-power img').fadeIn(500);
+    $('.power-on').hover(e => {
+        $('.power-on img').fadeIn(500);
     }, e => {
-        $('.fake-power img').fadeOut(500);
+        $('.power-on img').fadeOut(500);
     });
 
-    // class="animate_animated animate__zoomOut"
-    // for the button on click
 
     $('.fake-power').click(e => {
         $('.navbar').addClass('flexicution');
         $('.guise').fadeOut(800);
         $('.head').fadeOut(2000);
-        $('.power').fadeOut(1000);
-        $('.fake-power').fadeOut(1000);
+        $('.power-off').fadeOut(1000);
+        $('.power-on').fadeOut(1000);
         $('body').css({
             'overflow': 'visible',
             'overflow-x': 'hidden',
@@ -38,9 +37,6 @@ $('document').ready(function () {
     const nl = document.querySelectorAll('.navlink');
     const logo = document.querySelector('.the-light');
 
-
-
- 
 
     const navShrink = new IntersectionObserver(function(entries, navShrink) {
         entries.map(entry => {
